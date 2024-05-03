1becc6631c7cd9ca98ed0d80ab6d9cf95b276c31
Dabz <itzdabbzz@gmail.com>
2024-05-03 12:50:38 -0500

    Edit Bufferline Config

--- Neovim/lua/core/configure/bufferline.lua
+++ Neovim/lua/core/configure/bufferline.lua
@@ -24,7 +24,8 @@ bufferline.setup({
                 filetype = "neo-tree",
                 text = "File Explorer",
                 text_align = "center",
-                separator = true
+                separator = true,
+                highlight = "Directory"
             }
         },
         color_icons = true,
@@ -32,6 +33,6 @@ bufferline.setup({
         show_buffer_close_icons = true,
         show_close_icon = true,
         show_tab_indicators = true,
-
+        always_show_bufferline = true,
     }
 })
