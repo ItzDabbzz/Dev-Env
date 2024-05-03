d316eaa72ff3dbbd3cb5274870fc0b8c575240c4
Dabz <itzdabbzz@gmail.com>
2024-05-03 12:54:56 -0500

    Configure Plenary

--- Neovim/lua/plugins/misc.lua
+++ Neovim/lua/plugins/misc.lua
@@ -151,66 +151,84 @@ return {
         dependencies = {
             "akinsho/nvim-toggleterm.lua",
             "nvim-telescope/telescope.nvim",
-            "nvim-lua/plenary.nvim", -- only needed because it's a dependency of telescope
         },
         config = true,
     },
+    "nvim-lua/plenary.nvim",
     {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
+        event = "User FileOpened",
         opts = {
             keywords = {
                 FIX = {
-                    icon = require("helpers.icons").diagnostics.Bug .. " ", -- icon used for the sign, and in search results
-                    color = "error", -- can be a hex color, or a named color (see below)
-                    alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+                    icon = require("helpers.icons").diagnostics.Bug .. " F", -- icon used for the sign, and in search results
+                    color = "error",                                         -- can be a hex color, or a named color (see below)
+                    alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },              -- a set of other keywords that all map to this FIX keywords
                     -- signs = false, -- configure signs for some keywords individually
                 },
-                TODO = { icon = require("helpers.icons").ui.Note .. " ", color = "info" },
-                HACK = { icon = require("helpers.icons").ui.Fire .. " ", color = "warning" },
-                WARN = { icon = require("helpers.icons").diagnostics.Warning .. " ", color = "warning", alt = { "WARNING", "XXX" } },
-                PERF = { icon = require("helpers.icons").diagnostics.BoldQuestion .. " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-                NOTE = { icon = require("helpers.icons").diagnostics.Hint .. " ", color = "hint", alt = { "INFO" } },
+                TODO = { icon = require("helpers.icons").ui.Note .. " T", color = "info" },
+                HACK = { icon = require("helpers.icons").ui.Fire .. " H", color = "warning" },
+                WARN = { icon = require("helpers.icons").diagnostics.Warning .. " W", color = "warning", alt = { "WARNING", "XXX" } },
+                PERF = { icon = require("helpers.icons").diagnostics.BoldQuestion .. " P", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+                NOTE = { icon = require("helpers.icons").diagnostics.Hint .. " N", color = "hint", alt = { "INFO" } },
                 TEST = { icon = require("helpers.icons").diagnostics.BoldHint .. " ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
             },
             gui_style = {
-                fg = "NONE", -- The gui style to use for the fg highlight group.
-                bg = "BOLD", -- The gui style to use for the bg highlight group.
+                fg = "NONE",       -- The gui style to use for the fg highlight group.
+                bg = "BOLD",       -- The gui style to use for the bg highlight group.
             },
             merge_keywords = true, -- when true, custom keywords will be merged with the defaults
-            -- highlighting of the line containing the todo comment
-            -- * before: highlights before the keyword (typically comment characters)
-            -- * keyword: highlights of the keyword
-            -- * after: highlights after the keyword (todo text)
-            highlight = {
-                multiline = true,    -- enable multine todo comments
-                multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
-                multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
-                before = "",         -- "fg" or "bg" or empty
-                keyword = "wide",    -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-                after = "fg",        -- "fg" or "bg" or empty
-                pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
-                comments_only = true, -- uses treesitter to match keywords in comments only
-                max_line_len = 400,  -- ignore lines longer than this
-                exclude = {},        -- list of file types to exclude highlighting
-            },
-            -- list of named colors where we try to extract the guifg from the
-            -- list of highlight groups or use the hex color if hl not found as a fallback
-            colors = {
-                error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-                warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-                info = { "DiagnosticInfo", "#2563EB" },
-                hint = { "DiagnosticHint", "#10B981" },
-                default = { "Identifier", "#7C3AED" },
-                test = { "Identifier", "#FF00FF" }
-            },
-        }
+            -- highlighting of the line conta
+        },
     },
-    { -- TODO: Set this up
+
+    -- Highlight matched bracket
+    {
+        "monkoose/matchparen.nvim",
+        event = "User LazyFile",
+        opts = {},
+    },
+
+    -- Highlight undo/redo change
+    {
+        "tzachar/highlight-undo.nvim",
+        keys = {
+            { "u",     desc = "Undo" },
+            { "<C-R>", desc = "Redo" },
+            { "<C-Z>", "<Cmd>normal u<CR>", mode = "i", desc = "Undo" },
+        },
+        opts = {
+            -- Same as highlight on yank
+            duration = 150,
+            undo = { hlgroup = "Search" },
+            redo = { hlgroup = "Search", lhs = "U" },
+        },
+    },
+    {
+        -- TODO: Set this up
         "RRethy/vim-illuminate",
         event = "User FileOpened",
-        config = function()
+        config = function(_, opts)
+            require("illuminate").configure(opts)
 
+            local function map(key, dir, buffer)
+                vim.keymap.set("n", key, function()
+                    require("illuminate")["goto_" .. dir .. "_reference"](false)
+                end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+            end
+
+            map("]]", "next")
+            map("[[", "prev")
+
+            -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function()
+                    local buffer = vim.api.nvim_get_current_buf()
+                    map("]]", "next", buffer)
+                    map("[[", "prev", buffer)
+                end,
+            })
         end,
     },
     {
