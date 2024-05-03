6dee1acd771cc849777158785eda84fd0398244f
Dabz <itzdabbzz@gmail.com>
2024-05-03 12:55:21 -0500

    Noice Reconfigure

--- Neovim/lua/plugins/noice.lua
+++ Neovim/lua/plugins/noice.lua
@@ -10,56 +10,85 @@ return {
         -- OPTIONAL:
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
-        --"rcarriga/nvim-notify",
+        --"rcarriga/nvim-notify"
+        {
+            "rcarriga/nvim-notify",
+            keys = {
+                {
+                    "<leader>un",
+                    function()
+                        require("notify").dismiss({ silent = true, pending = true })
+                    end,
+                    desc = "Dismiss all Notifications",
+                },
+            },
+            opts = {},
+        },
     },
+    -- stylua: ignore
+    keys = {
+        { "<C-Enter>",  function() require("noice").redirect(vim.fn.getcmdline()) end,                 mode = "c",                 desc = "Redirect Cmdline" },
+        { "<Leader>nl", function() require("noice").cmd("last") end,                                   desc = "Noice Last Message" },
+        { "<Leader>nh", function() require("noice").cmd("history") end,                                desc = "Noice History" },
+        { "<Leader>na", function() require("noice").cmd("all") end,                                    desc = "Noice All" },
+        { "<Leader>nd", function() require("noice").cmd("dismiss") end,                                desc = "Dismiss All" },
+        { "<C-F>",      function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,              expr = true,              desc = "Scroll forward",  mode = { "i", "n", "s" } },
+        { "<C-B>",      function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,              expr = true,              desc = "Scroll backward", mode = { "i", "n", "s" } },
+    },
+    init = function()
+        vim.opt.cmdheight = 0
+    end,
     config = function()
         require("noice").setup({
             lsp = {
-              -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-              override = {
-                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                ["vim.lsp.util.stylize_markdown"] = true,
-                ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-              },
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                progress = {
+                    enabled = false,
+                },
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                },
             },
             -- you can enable a preset for easier configuration
             presets = {
-              bottom_search = true, -- use a classic bottom cmdline for search
-              command_palette = true, -- position the cmdline and popupmenu together
-              long_message_to_split = true, -- long messages will be sent to a split
-              inc_rename = false, -- enables an input dialog for inc-rename.nvim
-              lsp_doc_border = true, -- add a border to hover docs and signature help
+                bottom_search = false,         -- use a classic bottom cmdline for search
+                command_palette = true,       -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = true,           -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = true,        -- add a border to hover docs and signature help
             },
             views = {
                 cmdline_popup = {
-                  position = {
-                    row = 5,
-                    col = "50%",
-                  },
-                  size = {
-                    width = 60,
-                    height = "auto",
-                  },
+                    position = {
+                        row = 5,
+                        col = "50%",
+                    },
+                    size = {
+                        width = 60,
+                        height = "auto",
+                    },
                 },
                 popupmenu = {
-                  relative = "editor",
-                  position = {
-                    row = 8,
-                    col = "50%",
-                  },
-                  size = {
-                    width = 60,
-                    height = 10,
-                  },
-                  border = {
-                    style = "rounded",
-                    padding = { 0, 1 },
-                  },
-                  win_options = {
-                    winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-                  },
+                    relative = "editor",
+                    position = {
+                        row = 8,
+                        col = "50%",
+                    },
+                    size = {
+                        width = 60,
+                        height = 10,
+                    },
+                    border = {
+                        style = "rounded",
+                        padding = { 0, 1 },
+                    },
+                    win_options = {
+                        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+                    },
                 },
-              },
-          })
+            },
+        })
     end
 }
